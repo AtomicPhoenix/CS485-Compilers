95285000d4def74924fa50b85894b22a122d3244
PossiblyAI <imrana50280@gmail.com>
2025-03-08 00:38:04 -0500

    Still 7/15 what the heck

---
+++ PA3/PA3c1/default-arithmetic.cl
@@ -0,0 +1,11 @@
+class Main inherits IO {
+  main() : Object {
+    {
+        out_int((new Int)+5);
+        out_int((new Int)-5);
+        out_int((new Int)*5);
+        out_int((new Int)/5);
+        out_int(~(new Int));
+    }
+  };
+};
---
+++ PA3/test-errors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for file in ./PA3c1/*; do
+	# Extract the base name of the file (without extension)
+	base_name="${file%.*}"
+	# Extract file extension
+	extension="${file##*.}"
+
+	# Check if the file is a .cl file and if there's a corresponding .cl-input file
+	if [ "$extension" == "cl" ] && [ ! -f "$base_name.cl-input" ]; then
+		# Run the .cl file with the 'cool' program
+		# echo "Running: $file"
+		output=$(./cool "$file" | grep -i "ERROR")
+		if [ -n "$output" ]; then
+			printf "%s\n" "$output"
+		fi
+		# else
+		# Debugging output: if the file has a matching .cl-input file
+		# if [ -f "$base_name.cl-input" ]; then
+		# echo "Skipping: $file (corresponding .cl-input file exists)"
+		# fi
+	fi
+done
