bceed94c7f205c216be82436296679044427d1d7
Logan Kriebel <logan.kriebel@gmail.com>
2025-04-03 23:18:59 -0400

    add hella assembly implementations (probably nonworking :))

--- PA3/lib/asm.ml
+++ PA3/lib/asm.ml
@@ -94,6 +94,7 @@ in_int:\n
 
 
 let var_locations = Hashtbl.create 32
+let string_map = Hashtbl.create 32
 
 let add_var_addr (var_name : string) =
   let fp_offset = 8 * Hashtbl.length var_locations in
@@ -105,77 +106,138 @@ let get_var_addr (var_name : string) : string =
   | None ->
       Printf.fprintf out_file "; Is this supposed to happen?\n";
       var_name
+let string_counter = ref 0
 
 (** Method to convert a TAC element to assembly code *)
 let tac_to_as (tac : tac_elem) =
   match tac.operand with
   (* | Assignment *)
-  (* | Bt *)
+   | Bt ->
+      let arg1 = get_var_addr tac.arg1 in
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\ttestq %s, %s\n" "%rax" "%rax";
+      Printf.fprintf out_file "\tjne %s\n" tac.arg2
   | Call ->
       (* Push all variables onto stack *)
       (* Push all onto stack *)
       Printf.fprintf out_file "\tcallq %s\n" tac.arg1
-  (* | Comment *)
-  (* | Label *)
-  (* | Jmp *)
+  | Comment -> Printf.fprintf out_file "\t%s\n" ("#" ^ tac.arg1)
+  | Label -> Printf.fprintf out_file "\t%s\n" tac.arg1
+  | Jmp ->
+      Printf.fprintf out_file "\tjmp %s\n" tac.arg1
   (* | Case *)
   (* | Default *)
-  (* | Return *)
+  | Return ->
+      Printf.fprintf out_file "\tmovq %%rbp, %%rsp\n";
+      Printf.fprintf out_file "\tpopq %%rbp\n";
+      Printf.fprintf out_file "\tret\n"
   (* | LetNoInit *)
   (* | Ident_Expr of string *)
-   | New 
+  (*| New *)
   (* | Isvoid *)
   | Plus ->
       let arg1 = get_var_addr tac.arg1 in
       let arg2 = get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\taddw %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tmovw %s, %s\n" arg2 result
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\taddl %s, %s\n" arg2 "%eax";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rax" result
   | Minus ->
       let arg1 = get_var_addr tac.arg1 in
       let arg2 = get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\tsubw %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tmovw %s, %s\n" arg2 result
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tsubl %s, %s\n" arg2 "%eax";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rax" result
   | Divide ->
       let arg1 = get_var_addr tac.arg1 in
-      let arg2 = get_var_addr tac.arg2 in
+      let arg2 =get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\tdivw %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tmovw %s, %s\n" arg2 result
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tcltd\n";
+      Printf.fprintf out_file "\tidivl %s\n" arg2;
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rax" result
   | Times ->
       let arg1 = get_var_addr tac.arg1 in
       let arg2 = get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\tmulw %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tmovw %s, %s\n" arg2 result
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\timulll %s, %s\n" arg2 "%eax";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rax" result
   | LessThan ->
       let arg1 = get_var_addr tac.arg1 in
       let arg2 = get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\tcmp %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tjmp %s" result
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tcmpq %s, %s\n" arg1 arg2;
+      Printf.fprintf out_file "\tmovq %s, %s\n" result "%rdx";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "$0" "%rdx";
+      Printf.fprintf out_file "\tcmovlq %s, %s\n" "$1" "%rdx";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rdx" result
+
   | LessEqual ->
       let arg1 = get_var_addr tac.arg1 in
       let arg2 = get_var_addr tac.arg2 in
       add_var_addr tac.result;
       let result = get_var_addr tac.result in
-      Printf.fprintf out_file "\tcmp %s, %s\n" arg1 arg2;
-      Printf.fprintf out_file "\tjle %s" result
-  (* | Equal *)
-  (* | Not *)
-  (* | Negate *)
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tcmpq %s, %s\n" arg1 arg2;
+      Printf.fprintf out_file "\tmovq %s, %s\n" "$0" "%rdx";
+      Printf.fprintf out_file "\tcmovlq %s, %s\n" "$1" "%rdx";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rdx" result
+  | Equal ->
+      let arg1 = get_var_addr tac.arg1 in
+      let arg2 = get_var_addr tac.arg2 in
+      add_var_addr tac.result;
+      let result = get_var_addr tac.result in
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tcmpq %s, %s\n" "%rax" arg2;
+      Printf.fprintf out_file "\tmovq %s, %s\n" "$0" "%rdx";
+      Printf.fprintf out_file "\tcmoveq %s, %s\n" "$1" "%rdx";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rdx" result
+  | Not ->
+      let arg1 = get_var_addr tac.arg1 in
+      add_var_addr tac.result;
+      let result = get_var_addr tac.result in
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\ttestq %s, %s\n" "%rax" "%rax";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "$0" "%rdx";
+      Printf.fprintf out_file "\tcmoveq %s, %s\n" "$1" "%rdx";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rdx" result
+  | Negate ->
+      let arg1 = get_var_addr tac.arg1 in
+      add_var_addr tac.result;
+      let result = get_var_addr tac.result in
+      Printf.fprintf out_file "\tmovq %s, %s\n" arg1 "%rax";
+      Printf.fprintf out_file "\tnotq %s\n" "%rax";
+      Printf.fprintf out_file "\tmovq %s, %s\n" "%rax" result
   | Int_Constant ->
       Printf.fprintf out_file "\tpush %s\n" tac.arg1;
       add_var_addr tac.result
   | String_Constant ->
-      String.iter
-        (fun s -> Printf.fprintf out_file "\t.byte %d # %c\n" (int_of_char s) s)
-        tac.arg1
+      (*String.iter*)
+        (*(fun s -> Printf.fprintf out_file "\t.byte %d # %c\n" (int_of_char s) s)*)
+        (*tac.arg1*)
+      (match Hashtbl.find_opt string_map tac.arg1 with
+      | Some str_id -> Printf.fprintf out_file "\t%s\n" ("movq\t.string" ^ string_of_int(str_id) ^ ", %rsi")
+      | None -> (
+      string_counter := !string_counter + 1;
+      Hashtbl.add string_map tac.arg1 !string_counter;
+      Printf.fprintf out_file "\t%s\n" ("movq\t.string" ^ string_of_int(!string_counter) ^ ", %rsi")
+      
+      ))
+      (* This is all we do because they're emitted later :) *)
+      
+      (* This is the later but that's another stage; needs to be NOT just in an expression lm ao*)
+      (*Printf.fprintf out_file "\t%s\n" (".secton\t.rodata");*)
+      (*Printf.fprintf out_file "%s\n" ("string" ^ string_of_int(!string_counter) ^ ":");*)
+      (*Printf.fprintf out_file "\t%s\n" (".string \"" ^ string_of_int(!string_counter) ^ "\"");*)
+      (*Printf.fprintf out_file "\t%s\n" (".string \"" ^ string_of_int(!string_counter) ^ "\"")*)
+
   (* | Boolean_Constant *)
   | _ -> assert false
