9f949b7495c37069e52900a7be3309141b58adc4
PossiblyAI <imrana50280@gmail.com>
2025-04-06 02:51:20 -0400

    Fixed vtables

--- PA3/bin/main.ml
+++ PA3/bin/main.ml
@@ -33,32 +33,21 @@ let default_classes : Parser.annotated_ast_elem list =
     };
   ]
 
-
-
 let () =
-  (* let class_map = Parser.parse_class_map () in
-  let implementation_map = Parser.parse_implementation_map () in
-  let parent_map = Parser.parse_parent_map () in  *)
-  let annotated_ast = Parser.parse_annotated_ast () in
-  List.iter Tac.add_class default_classes;
-  List.iter Tac.add_class annotated_ast;
-
-  (*let tacs = Tac.parse_tac_expressions annotated_ast in*)
-  (*let cfg_list = List.map Cfg.tac_to_cfg tacs in*)
-  (*let asm_commands =*)
-    (*List.map cfg_to_asm cfg_list |> List.flatten |> List.flatten *)
-  (*in*)
-
-  (*List.iter Asm.print_asm asm_commands*)
   let vtables = Asm.create_default_vtables () @ !Asm.vtable_list in
   let print_vtable (table : Asm.vtable) =
     let name = table.name_id in
     let strid = table.name_string_id in
-    Printf.fprintf Print.out_file "\tglobl\t%s..vtable\n" name;
+    Printf.fprintf Print.out_file ".globl %s..vtable\n" name;
     Printf.fprintf Print.out_file "%s..vtable:\n" name;
     Printf.fprintf Print.out_file "\t.quad string%d\n" strid;
-    List.iter (fun (func : Asm.vtable_func) -> Printf.fprintf Print.out_file "\t.quad %s.%s\n" func.type_name func.method_name) table.methods;
-    Printf.fprintf Print.out_file "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"
+    List.iter
+      (fun (func : Asm.vtable_func) ->
+        Printf.fprintf Print.out_file "\t.quad %s.%s\n" func.type_name
+          func.method_name)
+      table.methods;
+    Printf.fprintf Print.out_file
+      "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"
   in
   List.iter print_vtable vtables;
   let print_new_funcs funcs =
@@ -69,18 +58,33 @@ let () =
       Printf.fprintf Print.out_file "\t.type\t%s..new, @function\n" name;
       List.iter (fun ln -> Asm.print_asm ln) lines;
       Printf.fprintf Print.out_file "\t.size\t%s, .-%s\n" name name;
-    Printf.fprintf Print.out_file "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"
+      Printf.fprintf Print.out_file
+        "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n"
     in
     List.iter print_new_func funcs
-  in print_new_funcs Asm.new_funcs;
-  List.iter (fun func -> List.iter (fun f -> Asm.print_asm f) func; Printf.fprintf Print.out_file "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n") Asm.intrinsic_funcs;
+  in
+  print_new_funcs Asm.new_funcs;
+  List.iter
+    (fun func ->
+      List.iter (fun f -> Asm.print_asm f) func;
+      Printf.fprintf Print.out_file
+        "\t#;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n")
+    Asm.intrinsic_funcs;
 
+  (* let class_map = Parser.parse_class_map () in
+  let implementation_map = Parser.parse_implementation_map () in
+  let parent_map = Parser.parse_parent_map () in  *)
+  let annotated_ast = Parser.parse_annotated_ast () in
+  List.iter Tac.add_class default_classes;
+  List.iter Tac.add_class annotated_ast;
 
+  (*let tacs = Tac.parse_tac_expressions annotated_ast in*)
+  (*let cfg_list = List.map Cfg.tac_to_cfg tacs in*)
+  (*let asm_commands =*)
+  (*List.map cfg_to_asm cfg_list |> List.flatten |> List.flatten *)
+  (*in*)
 
-      
-
-
-
+  (*List.iter Asm.print_asm asm_commands*)
 
   (* A List of basic blocks *)
   (* A list of list of tac elems *)
