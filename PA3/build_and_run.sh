65e67521dfb574618f68d409d72cff7cbb787c7c
PossiblyAI <imrana50280@gmail.com>
2025-04-06 18:08:54 -0400

    Fixed print to stdout

--- PA3/build_and_run.sh
+++ PA3/build_and_run.sh
@@ -11,4 +11,11 @@ cat lib/print.ml lib/parser.ml lib/tac.ml lib/cfg.ml lib/asm.ml bin/main.ml |
 
 ocamlc main.ml
 rm main.cm*
-./a.out "$1"
+cp "$1" .
+file=$(basename "$1" .cl-type)
+./a.out "$file.cl-type"
+
+gcc -static -fno-pie -g3 -o program "$file".s
+./program
+rm "./$file".cl* 2>/dev/null
+rm "./$file.s" 2>/dev/null
