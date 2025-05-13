338b78198686bb08a602650661c0aef2de57db86
PossiblyAI <imrana50280@gmail.com>
2025-05-13 04:53:34 -0400

    aaaa

--- make_in.sh
+++ make_in.sh
@@ -5,5 +5,14 @@ file="./in_files.txt"
 while IFS= read -r line; do
 	# Process the line here
 	echo "Line: $line"
-	mv "$line.cl" "in-$line.cl"
+	mv "./Tests/$line" "./Tests/in-$line"
 done <"$file"
+#
+# for file in "$1"/in-*; do
+# 	# Get the new file name by removing "in-"
+# 	base_name=$(basename "$file")
+# 	new_name="${base_name#in-}"
+#
+# 	# Move (rename) the file
+# 	mv "$file" "$1/$new_name"
+# done
